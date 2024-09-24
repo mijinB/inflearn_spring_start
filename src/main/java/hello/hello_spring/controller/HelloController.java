f083c84105abba504656d764dbc480eae58ab736
Bmijin <bmj13465@jtts.co.kr>
2024-09-24 14:58:02 +0900

    @ResponseBody / viewResolver VS HttpMessageConverter 차이 및 구조 개념

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -17,5 +18,32 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {    // 외부(웹)에서 파라미터를 받아서 사용한다.
         model.addAttribute("name", name);
         return "hello-template";
+    }
+
+    @GetMapping("hello-string")
+    @ResponseBody       // HTTP body부에 data(name)를 넣어주겠다는 의미이다. (template HTML 코드 없이 화면에 보여진다.)
+    public String helloString(@RequestParam("name") String name) {
+        return "hello " + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+
+    static class Hello {
+        // name은 private로 접근할 수 없고, getter setter로만 접근 가능하다.
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
