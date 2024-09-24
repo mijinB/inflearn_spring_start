8beea8159fab4fb91b5e7fbbdf3eb5973ce133ba
Bmijin <bmj13465@jtts.co.kr>
2024-09-24 14:28:06 +0900

    MVC와 템플릿 엔진 / web에서 param(url) 받아서 사용하기

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -10,5 +11,11 @@ public class HelloController {
     public String hello(Model model) {      // model ⇒ MVC
         model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {    // 외부(웹)에서 파라미터를 받아서 사용한다.
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
