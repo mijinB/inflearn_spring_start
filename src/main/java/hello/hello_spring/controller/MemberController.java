df34ff36aa26930bf2ef89d46bb181c0bc559f46
Bmijin <bmj13465@jtts.co.kr>
2024-09-26 14:48:25 +0900

    자바 코드로 직접 스프링 빈 등록하기

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -6,6 +6,16 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class MemberController {
+    // @Autowired private MemberService memberService;      이렇게 하는 필드 주입도 있지만 권장하지 않는다. IntelliJ IDE도 경고를 준다.
+
+    /* 이런 식으로 선언한 후에 세팅해주는 세터 주입도 있지만, 권장하지 않는다. 한번 세팅되면 수정할 일이 없는데 public으로 해줘야 하기 때문.
+    private MemberService memberService;
+    @Autowired
+    public void setMemberService(MemberService memberService) {
+        this.memberService = memberService;
+    }
+     */
+
     private final MemberService memberService;
 
     @Autowired
