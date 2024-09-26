10b3f96715e8da0a3a9f90380492f52e55d570c5
Bmijin <bmj13465@jtts.co.kr>
2024-09-26 16:00:53 +0900

    회원 목록 화면 UI & 회원 목록 api

--- src/main/java/hello/hello_spring/controller/MemberController.java
+++ src/main/java/hello/hello_spring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hello_spring.domain.Member;
 import hello.hello_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -39,5 +42,12 @@ public class MemberController {
         memberService.join(member); // 회원가입
 
         return "redirect:/";        // 홈 화면으로 redirect
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
