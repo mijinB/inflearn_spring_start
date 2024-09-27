d1a4f64e7c090cf0e5a77f54806acea03c0797cf
Bmijin <bmj13465@jtts.co.kr>
2024-09-27 16:12:06 +0900

    스프링 데이터 JPA 사용

--- src/main/java/hello/hello_spring/SpringConfig.java
+++ src/main/java/hello/hello_spring/SpringConfig.java
@@ -2,37 +2,40 @@ package hello.hello_spring;
 
 import hello.hello_spring.repository.*;
 import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.persistence.EntityManager;
 
 @Configuration
 public class SpringConfig {
 
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(EntityManager em) {
-        this.em = em;
+    @Autowired      // 생략 가능
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
-    @Bean
-    public MemberRepository memberRepository() {
-        /* Memory
-        return new MemoryMemberRepository(); */
-
-        /* 순수 JDBC
-        return new JdbcMemberRepository(dataSource); */
-
-        /* JDBC Template
-        return new JdbcTemplateMemberRepository(dataSource); */
-
-        /* JPA */
-        return new JpaMemberRepository(em);
-    }
+    
+    // 스프링 데이터 JPA 할 때 주석
+//    @Bean
+//    public MemberRepository memberRepository() {
+//        /* Memory
+//        return new MemoryMemberRepository(); */
+//
+//        /* 순수 JDBC
+//        return new JdbcMemberRepository(dataSource); */
+//
+//        /* JDBC Template
+//        return new JdbcTemplateMemberRepository(dataSource); */
+//
+//        /* JPA
+//        return new JpaMemberRepository(em); */
+//        
+//        /* 스프링 데이터 JPA는 memberService() Bean 수정 */
+//    }
 }
